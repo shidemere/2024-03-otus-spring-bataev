bfd5c0b69bf3c758c10d8be084f5fed7bfb32953
prometheus <tihopadaetsneg@gmail.com>
2024-05-31 18:25:29 +0300

    feat: homework 08
    remove unused import from hw 08

--- hw08-spring-data-no-sql/src/main/java/ru/otus/hw/service/BookServiceImpl.java
+++ hw08-spring-data-no-sql/src/main/java/ru/otus/hw/service/BookServiceImpl.java
@@ -2,7 +2,6 @@ package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.otus.hw.exception.EntityNotFoundException;
 import ru.otus.hw.model.Author;
 import ru.otus.hw.model.Book;
